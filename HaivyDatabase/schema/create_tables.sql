214206e336088259073b2ff43b3743dd74b7f7cb
dakbink <146689097+dacbinh@users.noreply.github.com>
2025-05-20 07:47:00 +0700

    Problem: 1. AccountDetail table doesn't have any link to auth.users. 2. Incorrect FK references of tables Patient and Staff 3. timestamps need to change to timestamptz Fixes: 1. AccountDetails tables's PK now references auth.users(id) with delete cascade enabled 2. Fixed FK references of Patient and Staff tables, they now have account_uid references auth.user(id) 3, Replaced timestamp with timestamptz Additionally: 1. Added [foreign key] before every references FK for readability 2. Deleted unnecessary comments, replace with more readability ones 3. Re-draw changes to project's ERD, updated to github

--- HaivyDatabase/schema/create_tables.sql
+++ HaivyDatabase/schema/create_tables.sql
@@ -1,50 +1,58 @@
---create the uuid random generate extension, (we had it installed but i'm doing it for sure ;))
+--create the uuid random generate extension, (we had it installed but i'm doing it for asurance ;))
 create extension if not exist "pgcrypto";
-
--- account ascosiated
+-----------------------------------
+-- account table 
 create table AccountDetails(-- contains basic information of an account
-  account_uid uuid primary key default gen_random_uuid(),
+  account_uid uuid references auth.users(id) on delete cascade,
   first_name varchar(50), -- can be null and dummy name is auto-generated
   last_name varchar(50), -- can be null and dummy name is auto-generated
   dob date, -- can be null
   profile_picrure text, -- can be null
-  phone varchar(13) not null
+  primary key (account_uid)
 );
---patient accociated
+-----------------------------------
+--patient table
 create table Patient(
   patient_uid uuid primary key default gen_random_uuid(), 
-  account_uid integer references AccountDetails(account_uid),
   anonymous_status boolean default true, -- when a patient has an account, can be use to set visibility of patient information
+  foreign key account_uid references auth.users(id)
 );
+-----------------------------------
+--staff table
 create table Staff(
   staff_id uuid primary key default gen_random_uuid(),
-  account_uid integer references AccountDetails(account_uid),
+  foreign key account_uid integer references auth.users(id),
   role varchar(20),
   join_date date,
   status boolean  
 );
+-----------------------------------
+--ticket table
 create table Ticket(
   ticket_id uuid primary key default gen_random_uuid(),
-  assigned_to integer references Staff(staff_id),
+  foreign key assigned_to integer references Staff(staff_id),
   date_created date,
   content text,
   status boolean
 );
+-----------------------------------
+--appointment table
 create table Appointment(
   appointment_id uuid primary key default gen_random_uuid(),
-  staff_id integer references Staff(staff_id), 
-  ticket_id integer references Ticket(ticket_id),
-  patient_id integer references Patient(patient_id),
+  foreign key staff_id integer references Staff(staff_id), 
+  foreign key ticket_id integer references Ticket(ticket_id),
+  patient_uid integer references Patient(patient_uid),
   created_date date,
   meeting_date date,
   content text,
   visibility boolean,
   status boolean
 );
---Doctor accociated
+-----------------------------------
+--Doctor specification 
 create table Specification(
   specification_id uuid primary key default gen_random_uuid(),
-  name varchar(50);
+  name varchar(50),
   achieved_date date,
   level integer
 );
@@ -53,9 +61,11 @@ create table Specification(
 
 create table DoctorSpecification(
   staff_id integer references Staff(staff_id),
-  specification_id integer references Specification(specification_id),
+  foreign key specification_id integer references Specification(specification_id),
   primary key (staff_id, specification_id),
 );
+-----------------------------------
+--doctor schedule
 create table DaySession(
   day_session varchar(20) primary key default gen_random_uuid(),
   start_time time,
@@ -76,8 +86,8 @@ create table DoctorSchedule(
   primary key (staff_id, day_of_week, day_session),
   foreign key (day_of_week, day_session) references WeekDay(day_of_week, day_session)
 );
+-----------------------------------
 -- --regimen ascociated
---version 1
 create type med_timing as enum ('empty stomach', 'before meal', 'with meal', 'after meal'); 
 /*
 explaination:
@@ -94,6 +104,8 @@ create table Medicine(
   is_available boolean,
   med_time med_timing
 );
+-----------------------------------
+--prescription table
 create table Prescription(
   prescription_id uuid primary key default gen_random_uuid(),
   name varchar(50),
@@ -101,14 +113,16 @@ create table Prescription(
 );
 create table PrescriptionDetail(
   prescription_detail_id uuid primary key default gen_random_uuid(),
-  prescription_id integer references Prescription(prescription_id),
-  medicine_id integer references Medicine(medicine_id),
-  start_time timestamp,
-  end_time timestamp,
+  foreign key prescription_id integer references Prescription(prescription_id),
+  foreign key medicine_id integer references Medicine(medicine_id),
+  start_time timestamptz,
+  end_time timestamptz,
   dosage float,
   interval integer,
   note text
 );
+-----------------------------------
+-- customized regimen
 create table CustomizedRegimen(
   cus_regimen_id uuid primary key default gen_random_uuid(),
   name varchar(50),
@@ -117,14 +131,16 @@ create table CustomizedRegimen(
 );
 create table CustomizedRegimenDetail(
   cus_regimen_detail_id uuid primary key default gen_random_uuid(),
-  cus_regimen_id integer references Regimen(cus_regimen_id),
-  prescription_id integer references Prescription(prescription_id),
+  foreign key cus_regimen_id integer references Regimen(cus_regimen_id),
+  foreign key prescription_id integer references Prescription(prescription_id),
   start_date date,
   end_date date,
   total_dosage float,
   frequency integer,
   note text
 );
+-----------------------------------
+-- original regimen
 create table Regimen(
   regimen_id uuid primary key default gen_random_uuid(),
   name varchar(50),
@@ -133,40 +149,22 @@ create table Regimen(
 );
 create table RegimenDetail(
   regimen_detail_id uuid primary key default gen_random_uuid(),
-  regimen_id integer references Regimen(regimen_id),
-  medicine_id integer references Medicine(medicine_id),
+  foreign key regimen_id integer references Regimen(regimen_id),
+  foreign key medicine_id integer references Medicine(medicine_id),
   start_date date,
   end_date date,
   total_dosage float,
   frequency integer,
   note text
 );
+-----------------------------------
+--patient medicine intake history
 create table IntakeHistory(
   intake_id uuid primary key default gen_random_uuid(),
-  patient_id integer references Patient(patient_id),
-  prescription_id integer references Prescription(prescription_id),
-  take_time timestamp,
+  foreign key patient_uid integer references Patient(patient_uid),
+  foreign key prescription_id integer references Prescription(prescription_id),
+  take_time timestamptz,
   missed boolean,
   note text,
   remind_inc_appointment boolean
 );
---version 2: failure
--- create table RegimenVersion(
---   regimen_version_id serial primary key,
---   version_number varchar(50),
---   create_date date,
---   status boolean
--- );
--- create table Regimen(
---   regimen_id serial primary key,
---   regimen_version_id integer references RegimenVersion(regimen_version_id),
---   name varchar(50),
---   description text,
---   create_date date,
---   status boolean
--- );
--- create table RegimenDetail(
---   regimen_detail_id serial primary key,
---   medicine_id integer references Medicine(medicine_id),
-
--- )

c33b8367f116607773d69aa558dc678e36715af4
uselessV <uselessliem@gmail.com>
2025-06-04 12:17:28 +0700

    minor fix

--- policy/staff_update_ticket.sql
+++ policy/staff_update_ticket.sql
@@ -1,11 +1,12 @@
 CREATE POLICY "staff_can_update_ticket" 
 ON public.ticket
-FOR UPDATE
+FOR UPDATE 
+TO authenticated
 USING (
-    EXISTS (
-        SELECT 1 
-        FROM staff s
-        WHERE s.staff_id = auth.uid() 
-        AND s.status = true
+    assigned_to IN (
+        SELECT staff_id 
+        FROM staff 
+        WHERE account_uid = auth.uid() 
+        AND status = true
     )
 );

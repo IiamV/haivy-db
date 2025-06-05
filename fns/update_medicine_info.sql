4975be8b8402c42dfcddd923cbaeb988c27cd829
dakbink <146689097+dacbinh@users.noreply.github.com>
2025-06-05 14:05:01 +0700

    Issue #28 Problem: missing check for role in CRUD functions Fixes: Added role check and basic error handling for FE, updates only affect CUD operations, R is still available for all roles

--- fns/update_medicine_info.sql
+++ fns/update_medicine_info.sql
@@ -6,7 +6,18 @@ CREATE OR REPLACE FUNCTION update_medication_info(
     med_time medicine_timing
 )
 RETURNS VOID AS $$
+DECLARE
+    user_role role_type;
 BEGIN
+-- take the role from authorized user
+    SELECT s.role
+    FROM staffs s
+    WHERE s.user_id = auth.uid()
+    INTO user_role;
+-- restrict operation to admin and manager only
+    IF user_role IS NULL or user_role NOT IN ('admin', 'manager') THEN
+    RAISE EXCEPTION 'You do not have permission to perform this action';
+    END IF;
     UPDATE Medicine m
     SET 
         m.name = med_name,
@@ -14,5 +25,9 @@ BEGIN
         m.is_available = med_availability,
         m.med_time = med_time
     WHERE m.medicine_id = med_id;
+--in case something else went wrong
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE EXCEPTION 'Something went wrong while trying to update medicine: %', SQLERRM;
 END;
 $$ LANGUAGE plpgsql;

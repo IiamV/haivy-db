5bde23c6dc4c1b4349f47a0dca7ac161b20e1818
uselessV <uselessliem@gmail.com>
2025-06-10 18:03:24 +0700

    issue 36
    create_regimen
    get_regimen_details
    query_regimens

---
+++ fns/get_regimen_details.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE FUNCTION public.get_regimen_details(
+    p_regimen_id uuid
+)
+RETURNS JSON AS $$
+BEGIN
+    RETURN (
+        SELECT to_json(array_agg(details))
+        FROM (
+            SELECT 
+                rd.*,
+                m.name AS medicine_name
+            FROM regimen_details rd
+            JOIN Medicines m ON rd.medicine_id = m.id
+            WHERE rd.regimen_id = p_regimen_id
+        ) AS details
+    );
+END;
+$$ LANGUAGE plpgsql;
